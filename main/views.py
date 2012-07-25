3e5fdf5e6f0dc9af8e1a9192a47337ecefecaddb
Eric Roberts <eric@entelechynt.com>
2012-07-25 10:26:49 -0400

    Added initial CSS

--- main/views.py
+++ main/views.py
@@ -2,7 +2,8 @@ from random import choice
 
 import gdata
 
-from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 import gdata.youtube
 import gdata.youtube.service
@@ -11,16 +12,30 @@ yt_service = gdata.youtube.service.YouTubeService()
 
 
 def test(request):
-    data = []
+    context_dict = {}
 
     feed = get_most_popular_youtube_feed()
-    video_info = get_random_youtube_video_info_from_feed(feed)
+    comment_info = get_random_youtube_video_info_from_feed(feed)
 
-    data.extend(video_info[0])
-    data.extend(video_info[1])
+    # Add comment into to our context dictionary
+    context_dict.update(comment_info)
+    context_dict["comment_context"] = get_random_comment_context_text()
 
-    str = '\n'.join(data)
-    return HttpResponse(str)
+    return render_to_response('main.html',
+                              context_dict,
+                              context_instance=RequestContext(request))
+
+
+def get_random_comment_context_text():
+    strings = (
+        "'s discussion on",
+        "'s insight on",
+        " discussing",
+        " on being inspired by",
+        " after carefully analyzing"
+    )
+
+    return choice(strings)
 
 
 def get_random_youtube_video_info_from_feed(feed):
@@ -31,19 +46,13 @@ def get_random_youtube_video_info_from_feed(feed):
     comment_feed = yt_service.GetYouTubeVideoCommentFeed(video_id=id)
     random_comment_entry = choice(comment_feed.entry)
 
-    video_title = get_youtube_feed_entry_title(random_video_entry)
-    video_url = get_youtube_feed_entry_url(random_video_entry)
+    comment_info = {}
+    comment_info["video_title"] = get_youtube_feed_entry_title(random_video_entry)
+    comment_info["video_url"] = get_youtube_feed_entry_url(random_video_entry)
+    comment_info["comment_author"] = get_comment_author_text_from_feed_entry(random_comment_entry)
+    comment_info["comment_text"] = get_comment_text_from_feed_entry(random_comment_entry)
 
-    video_info = (video_title, video_url)
-
-    comment_author = get_comment_author_text_from_feed_entry(random_comment_entry)
-    #comment_author_url = get_comment_author_url_from_feed_entry(random_comment_entry)
-    comment_content = get_comment_text_from_feed_entry(random_comment_entry)
-
-    #comment_info = (comment_author, comment_author_url, comment_content)
-    comment_info = (comment_author, comment_content)
-
-    return (video_info, comment_info)
+    return comment_info
 
 
 def get_most_popular_youtube_feed():
