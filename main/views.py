f7995eb76f0839eb85b98fdde615b1bb5de87745
Eric Roberts <eric@entelechynt.com>
2012-07-27 11:55:38 -0400

    Added 404 handing, permalink, and a next button

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ import gdata
 
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.http import Http404
 
 import gdata.youtube
 import gdata.youtube.service
@@ -11,13 +12,48 @@ import gdata.youtube.service
 yt_service = gdata.youtube.service.YouTubeService()
 
 
-def test(request):
+def default(request):
     context_dict = {}
 
     feed = get_most_popular_youtube_feed()
     comment_info = get_random_youtube_video_info_from_feed(feed)
 
     # Add comment into to our context dictionary
+    context_dict.update(comment_info)
+    context_dict["comment_context"] = get_random_comment_context_text()
+
+    return render_to_response('main.html',
+                              context_dict,
+                              context_instance=RequestContext(request))
+
+
+def permalink(request, video_id, comment_id):
+    context_dict = {}
+
+    try:
+        comment_info = get_youtube_video_info_from_ids(video_id, comment_id)
+    except:
+        raise Http404
+
+    context_dict.update(comment_info)
+    context_dict["comment_context"] = get_random_comment_context_text()
+
+    return render_to_response('main.html',
+                              context_dict,
+                              context_instance=RequestContext(request))
+
+
+def page_not_found():
+    context_dict = {}
+
+    comment_info = {}
+    comment_info["video_title"] = "Why can't I find this page?"
+    comment_info["video_url"] = "notreallyaurl"
+    comment_info["video_id"] = "404"
+    comment_info["comment_author"] = "404 Magic Man"
+    comment_info["comment_text"] = "This page doesn't exist. Seriously, this is a broken link."
+    comment_info["comment_id"] = "404"
+
     context_dict.update(comment_info)
     context_dict["comment_context"] = get_random_comment_context_text()
 
@@ -40,17 +76,36 @@ def get_random_comment_context_text():
 
 def get_random_youtube_video_info_from_feed(feed):
     random_video_entry = choice(feed.entry)
-
-    id = get_youtube_feed_entry_id(random_video_entry)
-
-    comment_feed = yt_service.GetYouTubeVideoCommentFeed(video_id=id)
+    comment_feed = yt_service.GetYouTubeVideoCommentFeed(
+        video_id=get_youtube_feed_entry_id(random_video_entry)
+    )
     random_comment_entry = choice(comment_feed.entry)
 
+    return get_youtube_video_info_from_entries(random_video_entry, random_comment_entry)
+
+
+def get_youtube_video_info_from_ids(video_id, comment_id):
+
+    #Get comment from URI provided in permalink
+    comment_uri = get_comment_uri_from_ids(video_id, comment_id)
+    try:
+        comment_entry = yt_service.GetYouTubeVideoCommentEntry(comment_uri)
+        video_entry = yt_service.GetYouTubeVideoEntry(video_id=video_id)
+    except:
+        raise Http404
+
+    return get_youtube_video_info_from_entries(video_entry, comment_entry)
+
+
+def get_youtube_video_info_from_entries(video_entry, comment_entry):
+
     comment_info = {}
-    comment_info["video_title"] = get_youtube_feed_entry_title(random_video_entry)
-    comment_info["video_url"] = get_youtube_feed_entry_url(random_video_entry)
-    comment_info["comment_author"] = get_comment_author_text_from_feed_entry(random_comment_entry)
-    comment_info["comment_text"] = get_comment_text_from_feed_entry(random_comment_entry)
+    comment_info["video_title"] = get_youtube_feed_entry_title(video_entry)
+    comment_info["video_url"] = get_youtube_feed_entry_url(video_entry)
+    comment_info["video_id"] = get_youtube_feed_entry_id(video_entry)
+    comment_info["comment_author"] = get_comment_author_text_from_feed_entry(comment_entry)
+    comment_info["comment_text"] = get_comment_text_from_feed_entry(comment_entry)
+    comment_info["comment_id"] = get_comment_entry_id(comment_entry)
 
     return comment_info
 
@@ -93,6 +148,19 @@ def get_comment_text_from_feed_entry(comment_entry):
 
 def get_comment_author_text_from_feed_entry(comment_entry):
     return comment_entry.author[0].name.text
+
+
+def get_comment_entry_id(comment_entry):
+    url = comment_entry.id.text
+    # Take last part of the URL for the id
+    id = url.split("/")[-1]
+    return id
+
+def get_comment_uri_from_ids(video_id, comment_id):
+    return "http://gdata.youtube.com/feeds/api/videos/" + \
+            video_id + \
+            "/comments/" + \
+            comment_id
 
 
 #def get_comment_author_url_from_feed_entry(comment_entry):
--- urls.py
+++ urls.py
@@ -6,13 +6,16 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'main.views.test')
+    url(r'^$', 'main.views.default'),
+    url(r'^permalink/(.*)/(.*)', 'main.views.permalink'),
     # url(r'^$', 'youtubeinsight.views.home', name='home'),
     # url(r'^youtubeinsight/', include('youtubeinsight.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admndocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+handler404 = 'main.views.page_not_found'
